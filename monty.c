5f0788270bf64d50aac43c456a63fc0088af169c
“Tristian <tristian.davis@atlasschool.com>
2024-08-04 14:07:30 -0500

    removed

--- monty.c
+++ monty.c
@@ -34,7 +34,7 @@ void push(stack_t **stack, int value, unsigned int line_number)
 	top = newnode;
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack)
 {
 	stack_t *temp = *stack;
 
