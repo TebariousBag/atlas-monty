a7ac72f8830629895548f7333fd4f17a5e267341
“Tristian <tristian.davis@atlasschool.com>
2024-08-04 16:16:28 -0500

    pall function change

--- monty.c
+++ monty.c
@@ -1,118 +1,109 @@
 #include "monty.h"
-
-/**
- * 
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 stack_t *stack = NULL;
 stack_t *top = NULL;
 
 void push(stack_t **stack, unsigned int line_number, const char *arg, int *error)
 {
-	stack_t *newnode = malloc(sizeof(stack_t));
-	if (newnode == NULL)
-	{
-		fprintf(stderr, "L%d: malloc failed\n", line_number);
-		*error = 1;
-		exit (EXIT_FAILURE);
-	}
+    stack_t *newnode = malloc(sizeof(stack_t));
+    if (newnode == NULL)
+    {
+        fprintf(stderr, "L%d: malloc failed\n", line_number);
+        *error = 1;
+        return;
+    }
 
-	newnode->n = atoi(arg);
+    newnode->n = atoi(arg);
 
-	if (newnode->n == 0 && strcmp(arg, "0") != 0)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		*error = 1;
-		return;
-	}
+    if (newnode->n == 0 && strcmp(arg, "0") != 0)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        *error = 1;
+        return;
+    }
 
-	newnode->prev = NULL;
-	newnode->next = *stack;
+    newnode->prev = NULL;
+    newnode->next = *stack;
 
-	if (*stack != NULL)
-	{
-		(*stack)->prev = newnode;
-	}
-	*stack = newnode;
-	top = newnode;
+    if (*stack != NULL)
+    {
+        (*stack)->prev = newnode;
+    }
+    *stack = newnode;
+    top = newnode;
 }
 
 void pall(stack_t **stack)
 {
-	stack_t *temp = *stack;
+    if (*stack == NULL)
+    {
+        printf("0\n");
+        return;
+    }
 
-	while (temp != NULL)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
+    printf("%d\n", (*stack)->n);
 }
-
-/**
- * main - start of program
- * argc: arg count
- * argv: arg vector
- * Return: success or failure
- */
 
 int main(int argc, char **argv)
 {
-	FILE *file; /*file stream*/
-	char *line = NULL; /*buffer to hold the lines*/
-	size_t len = 0; /*size of buffer*/
-	ssize_t read; /*what getline sees*/
-	unsigned int line_number = 0;
-	char *opcode;
-	char *arg;
-	int error = 0;
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 0;
+    char *opcode;
+    char *arg;
+    int error = 0;
 
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: monty file\n");
+        return EXIT_FAILURE;
+    }
 
-	if (argc != 2) /*make sure there is a file*/
-	{
-		fprintf(stderr, "Usage: monty file\n");
-		return (EXIT_FAILURE);
-	}
+    file = fopen(argv[1], "r");
 
-	file = fopen(argv[1], "r"); /*open the file for reading*/
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
 
-	if (file == NULL) /*errror opening*/
-	{
-		perror("Error opening file");
-		return (EXIT_FAILURE);
-	}
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        opcode = strtok(line, " ");
+        arg = strtok(NULL, " ");
 
-	while ((read = getline(&line, &len, file)) != -1) /*read each line of file*/
-	{
-		opcode = strtok(line, " "); /*tokenize line based off spaces*/
-		arg = strtok(NULL, " ");
+        if (strcmp(opcode, "push") == 0 && arg != NULL)
+        {
+            push(&stack, line_number, arg, &error);
+            if (!error)
+            {
+                line_number++;
+            }
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack);
+            line_number++;
+        }
+        else
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            error = 1;
+        }
 
-		if (strcmp(opcode, "push") == 0 && arg != NULL)
-		{
-			push(&stack, line_number, arg, &error);
-			if (!error)
-			{
-			line_number++;
-			}
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(&stack);
-			line_number++;
-		}
-		else
-		{
-			fprintf(stderr, "L%d: usage: push int\n", line_number);
-			error = 1;
-		}
+        if (error)
+        {
+            continue;
+        }   
+    }
 
-		if (error)
-		{
-			continue;
-		}	
-	}
+    fclose(file);
+    free(line);
 
-	fclose(file);
-	free(line);
-
-	return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
