efab8e7fabcbc4fb79e9363ce4acbeae8caac15f
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 13:24:34 -0500

    pint error

--- monty.c
+++ monty.c
@@ -5,6 +5,18 @@
 
 stack_t *stack = NULL;
 
+void pint(stack_t **stk, unsigned int line_number)
+{
+	if (*stk == NULL)
+	{
+		line_number++;
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+		fprintf(stdout, "%d\n", (*stk)->n);
+}
+
 void push(stack_t **stack, unsigned int line_number)
 {
 	int value;
