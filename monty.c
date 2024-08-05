d6691bd7d02df6a43c57559cb2fbcc62cb1db4f9
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 12:52:03 -0500

    revert

--- monty.c
+++ monty.c
@@ -5,22 +5,24 @@
 
 stack_t *stack = NULL;
 
-void push(stack_t **stack, unsigned int line_number, char *arg)
+void push(stack_t **stack, unsigned int line_number)
 {
-    int value;
-    if (!arg)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	int value;
+	char *arg = strtok(NULL, " \t\n");
 
-    value = atoi(arg);
+	if (arg == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(arg);
 
     stack_t *newnode = malloc(sizeof(stack_t));
-    if (!newnode)
+
+    if (newnode == NULL)
     {
         fprintf(stderr, "L%d: malloc failed\n", line_number);
-        freestack(stack);
         exit(EXIT_FAILURE);
     }
 
@@ -34,30 +36,16 @@ void push(stack_t **stack, unsigned int line_number, char *arg)
     *stack = newnode;
 }
 
-void pall(stack_t **stack, unsigned int line_number, char *arg)
+void pall(stack_t **stack, unsigned int line_number)
 {
-    (void)arg; // Arg is not used in this function
-    stack_t *current = *stack;
+	(void)line_number;
+   stack_t *current = *stack;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
-
-char *parse_line(char *line)
-{
-    char *opcode = strtok(line, " \t\n");
-    char *args = strtok(NULL, " \t\n");
-
-    // Concatenate opcode and args into a single string
-    char *command = malloc(strlen(opcode) + strlen(args) + 2); // +2 for spaces and null terminator
-    strcpy(command, opcode);
-    strcat(command, " ");
-    strcat(command, args ? args : "");
-
-    return command;
+   while (current)
+   {
+	printf("%d\n", current->n);
+	current = current->next;
+   }
 }
 
 int main(int argc, char *argv[])
@@ -68,7 +56,7 @@ int main(int argc, char *argv[])
     size_t len = 0;
     unsigned int line_number = 0;
     unsigned int i;
-    char *command;
+    char *opcode;
 
     if (argc != 2)
     {
@@ -77,6 +65,7 @@ int main(int argc, char *argv[])
     }
 
     file = fopen(argv[1], "r");
+
     if (file == NULL)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -93,37 +82,25 @@ int main(int argc, char *argv[])
     while ((getline(&line, &len, file)) != -1)
     {
         line_number++;
-        command = parse_line(line);
+        opcode = strtok(line, " \t\n");
 
-        // Manually parse the opcode and arguments
-        char *opcode = strtok(command, " ");
-        char *args = strchr(command, ' ') ? strdup(strchr(command, ' ') + 1) : NULL;
-
-        if (!opcode || strcmp(opcode, "push") != 0)
+        if (opcode == NULL)
         {
             line_number++;
             fprintf(stderr, "L%d: usage: push integer\n", line_number);
             exit(EXIT_FAILURE);
         }
-
-        if (args && strcmp(args, "") == 0)
+       for (i = 0; instructions[i].opcode; i++)
+       {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
         {
-            fprintf(stderr, "L%d: missing argument for push\n", line_number);
-            exit(EXIT_FAILURE);
+            instructions[i].f(&stack, line_number);
+            break;
         }
+       }
 
-        // Now you can use opcode and args as needed
-        // For example, to push an integer:
-        if (strcmp(opcode, "push") == 0)
-        {
-            int value = atoi(args);
-            // Call push function or equivalent logic here
-        }
-
-        free(command);
-        free(args); // Don't forget to free the dynamically allocated args
-        free(line);
-    }
+       free(line);
+	}
 
     fclose(file);
     freestack(&stack);
