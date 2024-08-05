9236aeabce2418e03f27fa8e9d9dea99534f7577
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 10:48:59 -0500

    value

--- monty.c
+++ monty.c
@@ -11,7 +11,7 @@ void push(stack_t **stack, unsigned int line_number)
     stack_t *newnode = malloc(sizeof(stack_t));
 	char *arg = strtok(NULL, " ");
 
-	if (!value)
+	if (arg == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
