0b7a28749d162dfeaf4814861bb28a27aa82c8cc
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 14:07:17 -0500

    removed an incrementer to line count

--- pop.c
+++ pop.c
@@ -9,7 +9,6 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		line_number++;
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
