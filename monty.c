1d0c35d8bdff63c6b1c668a88c746aafdecc1416
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 09:49:21 -0500

    cleaning up unused variables

--- monty.c
+++ monty.c
@@ -52,8 +52,7 @@ int main(int argc, char *argv[])
     unsigned int line_number = 0;
     char *opcode;
     char *arg;
-    int error = 0;
-
+	
     if (argc != 2)
     {
         fprintf(stderr, "Usage: monty file\n");
