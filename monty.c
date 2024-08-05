c7aedba396ddfb800aa520030f88344d7a651e3e
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 12:20:12 -0500

    while != NULL

--- monty.c
+++ monty.c
@@ -41,7 +41,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;
    stack_t *current = *stack;
 
-   while (current)
+   while (current != NULL)
    {
 	printf("%d\n", current->n);
 	current = current->next;
