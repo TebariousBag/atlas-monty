f04799e5d075b952c01031f4b848f11e5e46a57a
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 12:49:58 -0500

    testing

--- monty.c
+++ monty.c
@@ -5,11 +5,9 @@
 
 stack_t *stack = NULL;
 
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number, char *arg)
 {
     int value;
-    char *arg = strtok(NULL, " \t\n");
-
     if (!arg)
     {
         fprintf(stderr, "L%d: usage: push integer\n", line_number);
@@ -36,9 +34,9 @@ void push(stack_t **stack, unsigned int line_number)
     *stack = newnode;
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number, char *arg)
 {
-    (void)line_number;
+    (void)arg; // Arg is not used in this function
     stack_t *current = *stack;
 
     while (current != NULL)
