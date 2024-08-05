ac93a292b58d343416314261efc0a3769ddc10b1
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 11:42:26 -0500

    error messages

--- monty.c
+++ monty.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
 
     if (file == NULL)
     {
-        perror("Error opening file");
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
