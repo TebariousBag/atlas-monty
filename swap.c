097c107a90192b297e0f1fad1521ac3dc532ad6e
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 14:12:43 -0500

    incrementing my line_number

--- swap.c
+++ swap.c
@@ -11,7 +11,6 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		line_number++;
 		fprintf(stderr, "L%d: can't swap, stack too short\n",
 			line_number);
 		freestack(stack);
