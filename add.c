2e99a6649d1eae1340f102f83b7edb44435883e6
“Tristian <tristian.davis@atlasschool.com>
2024-08-05 19:25:27 -0500

    revert again, chasing checker points

--- add.c
+++ add.c
@@ -7,12 +7,6 @@
 
 void add(stack_t **stack, unsigned int line_number)
 {
-	int sum;
-	int val1;
-	int val2;
-
-	stack_t *temp;
-
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n",
@@ -21,14 +15,7 @@ void add(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	val1 = (*stack)->n;
-	val2 = (*stack)->next->n;
-
-	temp = (*stack)->next;
-	(*stack)->next = temp->next;
-	temp->next = NULL;
-
-	sum = val1 + val2;
-
-	push(sum, stack, line_number);
+	pop(stack, line_number);
+	pop(stack, line_number);
+	push(stack, line_number);
 }
